097fd44df9e7057ba27ae5b00d1be51ee10f8af0
Robotwaterfall <jaysonwelshman@gmail.com>
2025-05-01 21:11:19 -0400

    Created files for driving the robot

---
+++ BattleBotJCRtournament/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "TeleOpertatedMode")
+public class RobotContainer extends CommandOpMode {
+
+    public void initialize(){
+
+    }
+}
