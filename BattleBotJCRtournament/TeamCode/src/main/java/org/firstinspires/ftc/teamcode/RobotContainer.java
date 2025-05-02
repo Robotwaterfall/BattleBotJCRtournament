e4c3def2b6e8a4685493ebf7fdaed662f3aa32ab
Robotwaterfall <jaysonwelshman@gmail.com>
2025-05-02 09:42:38 -0400

    Merged omni-directional-drive into blade-Motor-Testing in a separate branch for testing.

--- BattleBotJCRtournament/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ BattleBotJCRtournament/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Commands.bladeMotorCommand;
 import org.firstinspires.ftc.teamcode.Commands.omniDriveCommand;
+import org.firstinspires.ftc.teamcode.Subsystems.bladeMotorSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.omniDriveSubsystem;
 
 @TeleOp(name = "TeleOperatedMode")
@@ -11,14 +13,20 @@ public class RobotContainer extends OpMode {
   omniDriveSubsystem driveSub;
   omniDriveCommand driveCommand;
 
+  bladeMotorSubsystem bladeSub;
+  bladeMotorCommand bladeCommand;
+
 
   public void init(){
     driveSub = new omniDriveSubsystem(hardwareMap);
-
     driveCommand = new omniDriveCommand(driveSub, gamepad1);
+
+    bladeSub = new bladeMotorSubsystem(hardwareMap);
+    bladeCommand = new bladeMotorCommand(bladeSub, gamepad1);
   }
 
   public void loop(){
     driveCommand.run();
+    bladeCommand.runBlade();
   }
 }
